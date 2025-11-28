97db90eb3cf1bc2fd9638025282bda509d3505d0
Teriyaki-Kun <michhu1212@gmail.com>
2025-11-28 19:49:47 +0700

    NavBar

--- wisatacandi/lib/main.dart
+++ wisatacandi/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisatacandi/screens/favorite_screen.dart';
 import 'package:wisatacandi/screens/home_screen.dart';
 import 'package:wisatacandi/screens/profile_screens.dart';
 import 'package:wisatacandi/screens/search_screen.dart';
@@ -31,24 +32,70 @@ class MainApp extends StatelessWidget {
     //   ),
     //   useMaterial3: true,
     // ),
-    home: HomeScreen(),
+    // home: HomeScreen(),
+    home: MainScreen(),
     );  
     //   home: DetailScreen(candi: candiList[0]),
     // );
   }
 }
 
-// class MainScreen extends StatefulWidget {
-//   const MainScreen({super.key});
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
-//   @override
-//   State<MainScreen> createState() => _MainScreenState();
-// }
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
 
-// class _MainScreenState extends State<MainScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Placeholder();
-//   }
-// }
+class _MainScreenState extends State<MainScreen> {
+  // TODO 1. Deklarasi Variabel
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomeScreen(),
+    SearchScreen(),
+    ProfileScreens(),
+    FavoriteScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // TODO 2. Buat Property Body berupa widget yang akan di tampilkan
+      body: _children[_currentIndex],
+      // TODO 3. Buat Bottom Navbar
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(canvasColor: Colors.deepPurple[100]), 
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index){
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorite',
+            ),
+          ],
+          selectedItemColor: Colors.pink[200],
+          unselectedItemColor: Colors.white,
+          showUnselectedLabels: true,
+        )
+      // TODO 4. Buat data dan child
+      ),
+    );
+  }
+}
 
--- wisatacandi/lib/widgets/item_card.dart
+++ wisatacandi/lib/widgets/item_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisatacandi/models/candi.dart';
+import 'package:wisatacandi/screens/detail_screen.dart';
 
 class ItemCard extends StatelessWidget {
   // TODO 1. Deklarasi Variabel Yang Akan Di Gunakan
@@ -9,51 +10,59 @@ class ItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO 6. Implementasi Routing ke Detail Screen
-    return Card(
-      // TODO 2. Tetapkan Parameter shape, margin , dan elevation dari Card
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      margin: EdgeInsets.all(4),
-      elevation: 1,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded( 
-            // TODO 7. Implementasi Hero Animation
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: 
-                // TODO 3. Buat Image Sebagai Anak Dari Column
-                Image.asset(
-                  candi.imageAsset,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => DetailScreen(candi: candi))
+        );
+      },
+      child: Card(
+        // TODO 2. Tetapkan Parameter shape, margin , dan elevation dari Card
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        margin: EdgeInsets.all(4),
+        elevation: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded( 
+              // TODO 7. Implementasi Hero Animation
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: 
+                  // TODO 3. Buat Image Sebagai Anak Dari Column
+                  Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+              ),
+            ),
+            // TODO 4. Buat Text Sebagai Anak Dari 
+            Padding(
+              padding: const EdgeInsets.only(left: 16, top: 8),
+              child: Text(
+                candi.name,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-            ),
-          ),
-          // TODO 4. Buat Text Sebagai Anak Dari 
-          Padding(
-            padding: const EdgeInsets.only(left: 16, top: 8),
-            child: Text(
-              candi.name,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          // TODO 5. Buat Text Sebagai Anak Dari Column
-          Padding(
-            padding: const EdgeInsets.only(left: 16, top: 8),
-            child: Text(
-              candi.type,
-              style: TextStyle(
-                fontSize: 12,
+            // TODO 5. Buat Text Sebagai Anak Dari Column
+            Padding(
+              padding: const EdgeInsets.only(left: 16, top: 8),
+              child: Text(
+                candi.type,
+                style: TextStyle(
+                  fontSize: 12,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
